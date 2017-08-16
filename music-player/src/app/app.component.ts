e31ab2504d30b61a7eaf0b57ad855ec9f9ed3a0c
DavAna <david@anaton.fr>
2017-08-17 00:42:51 +0300

    Added more functionalities
    Random & Loop

--- music-player/src/app/app.component.ts
+++ music-player/src/app/app.component.ts
@@ -11,8 +11,11 @@ import { MusicService } from '../music/music.service';
 export class AppComponent implements OnInit {
     title = 'Music Player';;
     musics: Music[];
-    selectedMusic: Music;
+    selectedMusic: Music = {id: undefined, name:""};
     playing:boolean = false;
+    random:boolean = false;
+    private playingOrder:number[] = [];
+    loop:boolean = false;
 
     constructor(private musicService: MusicService) { }
     ngOnInit(): void {
@@ -23,6 +26,10 @@ export class AppComponent implements OnInit {
         this.musicService.getAjaxMusics().then(musics => {
             this.musics = musics;
             this.selectedMusic = this.musics[0];
+
+            for(var i = 0; i < this.musics.length; i++){
+                this.playingOrder.push(i);
+            }
         });
     }
 
@@ -33,15 +40,41 @@ export class AppComponent implements OnInit {
         this.playing = false;
     }
     previousClicked():void {
-        var newId = this.selectedMusic.id - 2;
-        if(newId >= 0){
-            this.selectedMusic = this.musics[newId];
-        }
+        this.selectedMusic = this.musics[this.getIndex(this.selectedMusic.id, -1)];
     }
     nextClicked():void {
-        var newId = this.selectedMusic.id;
-        if(newId < this.musics.length){
-            this.selectedMusic = this.musics[newId];
+        this.selectedMusic = this.musics[this.getIndex(this.selectedMusic.id, +1)];
+    }
+    private getIndex(current:number, increment:number):number{
+        var oldIndex = current - 1; // Id start at 1, indexes start at 0
+        var playingIndex = this.playingOrder.indexOf(oldIndex);
+        var newPlayingIndex = playingIndex + increment;
+        if(this.loop){
+            newPlayingIndex %= this.playingOrder.length;
         }
+        if(newPlayingIndex >= 0 && newPlayingIndex < this.musics.length){
+            return this.playingOrder[newPlayingIndex];
+        }
+        else{
+            return oldIndex;
+        }
+    }
+    randomClicked(): void{
+        this.random = !this.random;
+        this.playingOrder = [];
+        for(var i = 0; i < this.musics.length; i++){
+            this.playingOrder.push(i);
+        }
+        if(this.random){
+            for (var i = this.playingOrder.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = this.playingOrder[i];
+                this.playingOrder[i] = this.playingOrder[j];
+                this.playingOrder[j] = temp;
+            }
+        }
+    }
+    loopClicked(): void{
+        this.loop = !this.loop;
     }
 }
